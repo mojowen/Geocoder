8e023378501e158ae246794afa34998020e1691d
Scott Duncombe <srduncombe@gmail.com>
2012-11-16 16:52:27 -0800

    adding google analytics + events

--- data/content/script/main.js
+++ data/content/script/main.js
@@ -26,6 +26,8 @@ $(document).on('click','button',function() {
 	addresses = $('textarea#addresses').val().split(/\n/);
 	if( addresses.length == 1 && addresses[0] == '' )  { $('.progress').text('Ummm nothing there, sport'); return false }
 
+	_gaq.push(['_trackEvent', 'Look Up', 'Start', addresses.length]);
+
 	$('table').html('');
 	$('.progress').text('Processed '+fixed.length+' out of '+(addresses.length+fixed.length))
 	newAddress( false,
@@ -48,12 +50,26 @@ $(document).on('click','button',function() {
 		function() { 
 			$('.progress').text('Completed '+fixed.length+' out of '+(addresses.length+fixed.length) )
 			$('.right em').text('Results - click for copyable result')
+			_gaq.push(['_trackEvent', 'Look Up', 'Finished', addresses.length]);
 		} 
 	);
 })
 .on('hover','table tr',function() {
-	var latlng = $('td:last',this).text();
+	var address = $('td:first',this).text();
+	latlng = $('td:last',this).text();
 	$('body').css('background-image','URL("http://maps.googleapis.com/maps/api/staticmap?sensor=true&center='+latlng+'&zoom=14&size=800x800&maptype=terrain")')
+	_gaq.push(['_trackEvent', 'Results', 'Hover', 'Hovered over '+address]);
 })
-.on('click','.right',function() { $('table').hide(); $('#results').show().select(); })
-.on('mouseout','#results',function() { $(this).hide(); $('table').show(); })
+.on('click','.right',function() { 
+	$('table').hide(); $('#results').show().select();
+	_gaq.push(['_trackEvent', 'Results', 'Click', 'Clicked on Table']);
+})
+.on({
+	mouseout: function() { 
+		$(this).hide(); $('table').show();
+		_gaq.push(['_trackEvent', 'Results', 'Hover-Out', 'Hovered Out on Table',false]);
+	},
+	copy: function() {
+		_gaq.push(['_trackEvent', 'Results', 'Copy', 'Copied the results']);
+	}
+},'#results')
